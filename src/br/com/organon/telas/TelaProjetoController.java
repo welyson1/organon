06a58470ce5a8ee15649ef9ce950c31b152093e3
welysondev <monitoriawel@hotmail.com>
2022-05-25 22:32:38 -0300

    Carregar Lista de projetos

--- src/br/com/organon/telas/TelaProjetoController.java
+++ src/br/com/organon/telas/TelaProjetoController.java
@@ -4,7 +4,10 @@
  */
 package br.com.organon.telas;
 
+import br.com.organon.DAO.ProjetoDAO;
+import br.com.organon.classes.Projeto;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -32,7 +35,7 @@ public class TelaProjetoController implements Initializable {
     @FXML
     private TextArea txtDescricao;
     @FXML
-    private ChoiceBox<?> choiseBoxEscolherProjeto;
+    private ChoiceBox<String> choiseBoxEscolherProjeto;
     @FXML
     private Button btnEditarProjeto;
     @FXML
@@ -40,14 +43,37 @@ public class TelaProjetoController implements Initializable {
     @FXML
     private Button btnSalvarProjeto;
     
+    //Criar um array de string para carregar os valores do seletor de projetos
+    private String[] projetos = {"Projeto a","Projeto b"}; 
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        //Carrega o array de string com os valores dos responsaveis
+        choiseBoxEscolherProjeto.getItems().addAll(att());
+        
+        //Ativa o retorno do responsavel
+        choiseBoxEscolherProjeto.setOnAction(this::getProjetolInterface);
+    } 
+    
+    //Função de retorno da seleção do responsavel pelo usuario
+    public void getProjetolInterface(javafx.event.ActionEvent event){
+        //Mostra o returno no console (String)
+        System.out.print(choiseBoxEscolherProjeto.getValue()); 
+    }
     
     //Ao clicar no botão EDITAR na interface essa função é chamada
     @FXML
     private void funcaoEditarProjeto(ActionEvent event) {
+        
+    }
+    private ArrayList<String> att(){
+        ProjetoDAO pDAO = new ProjetoDAO();
+        ArrayList<Projeto> laland = pDAO.buscarTodos();
+        ArrayList<String> sLista = new ArrayList();
+        for(Projeto p: laland){
+            sLista.add(p.getNome());
+        }
+        return sLista;
     }
     
     //Ao clicar no botão EXCLUIR na interface essa função é chamada
@@ -58,6 +84,25 @@ public class TelaProjetoController implements Initializable {
     //Ao clicar no botão SALVAR na interface essa função é chamada
     @FXML
     private void funcaoSalvarProjeto(ActionEvent event) {
+        System.out.print("Entrou");
+        try{
+            Projeto p = new Projeto();
+
+            ProjetoDAO pDAO = new ProjetoDAO();
+            
+            ArrayList<String> array = new ArrayList();
+
+            p.setNome(txtNomeProjeto.getText());
+            p.setRepositorio(txtLinkRepositorio.getText());
+            p.setLinguagem(txtLinguagens.getText());
+            p.setMdlProcess(txtModProcesso.getText());
+            p.setDescricao(txtDescricao.getText());
+            p.setDevs(array);
+
+            pDAO.criar(p);
+        }catch(Exception e){
+            System.out.print("Foi não");
+        }
     }
     
 }
