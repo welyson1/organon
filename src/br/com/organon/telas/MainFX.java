027a5e810561f54c0f3c2ca423caaa83d1800b6e
Patrick <patrickquine@alunos.utfpr.edu.br>
2022-05-30 15:27:45 -0300

    Tela de Login

--- src/br/com/organon/telas/MainFX.java
+++ src/br/com/organon/telas/MainFX.java
@@ -5,6 +5,7 @@
     import javafx.scene.Parent;
     import javafx.scene.Scene;
     import javafx.stage.Stage;
+import javafx.stage.StageStyle;
     
     
     public class MainFX extends Application {
@@ -12,9 +13,10 @@
         @Override
         public void start(Stage stage) throws Exception {            
             //Criação do palco e cena (Esse parte inicia a tela inicial)
-            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("TelaBoard.fxml"));
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("Login.fxml"));
             
-            stage.setTitle("Organon (Gerencie suas tarefas)");
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setTitle("Organon");
             Parent root = loader.load();
             stage.setScene(new Scene(root));
             stage.show();
