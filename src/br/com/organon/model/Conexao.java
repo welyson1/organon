5b3472ba5bfb4f02c79a9c283be1b85fb1012d53
Welyson <53984581+welyson1@users.noreply.github.com>
2023-03-16 23:48:42 -0300

    video e primeiro caso de teste

--- src/br/com/organon/model/Conexao.java
+++ src/br/com/organon/model/Conexao.java
@@ -3,13 +3,24 @@ import java.sql.*;
 
 public class Conexao {
         
-        private static final String driver = "com.mysql.cj.jdbc.Driver";
-        private static final String url = "jdbc:mysql://sql10.freesqldatabase.com:3306/sql10603220";
-        private static final String user = "sql10603220";
+        /*private static final String driver = "com.mysql.cj.jdbc.Driver";
+        private static final String url = "jdbc:db.vaurghltkbqbntucywaz.supabase.co:5432";
+        private static final String user = "postgres";
         private static final String pass = "u3xCXdhD5C";
+        //Senha ofvBQ2Nc9kWS1td4*/
+        private static final String url = "jdbc:postgresql://db.vaurghltkbqbntucywaz.supabase.co:5432/postgres?user=postgres&password=ofvBQ2Nc9kWS1td4";
     
         public static Connection conexao(){
-            Connection  conn = null;
+            Connection conn = null;
+            try {
+                conn = DriverManager.getConnection(url);
+                // fazer operações no banco de dados
+            } catch (SQLException e) {
+                // tratar exceção
+            }
+            return conn;
+        }
+           /* Connection  conn = null;
             try{
                 Class.forName(driver);
                 conn = DriverManager.getConnection(url,user,pass);
@@ -17,7 +28,7 @@ public class Conexao {
                 System.out.println("Erro Conexao" + e);
              }
            return conn;
-        }
+        }*/
          public static void main (String args[]) throws SQLException{
              Connection conexaoTst = conexao();
              if(conexaoTst!=null){
